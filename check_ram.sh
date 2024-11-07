703049c18d1c1b5e3fe77e2d4a3694252b6a437e
Guillaume Daguet <g.daguet@ateme.com>
2024-11-07 10:06:56 +0100

    Fix coments position

--- check_ram.sh
+++ check_ram.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# Remplace X par le pourcentage seuil
+# Pourcentage seuil
 SEUIL=90
 
-# Obtenir l'utilisation actuelle de la RAM en pourcentage
-
-
-# Comparer avec le seuil
-
 while true; do
-
+    # Obtenir l'utilisation actuelle de la RAM en pourcentage
     USAGE=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+    # Comparer avec le seuil    
     if (( $(echo "$USAGE > $SEUIL" | bc -l) )); then
-
         notify-send "/!\ Alerte RAM" "L'utilisation de la RAM est de ${USAGE}%"
     fi
-    
     sleep 10
 done
